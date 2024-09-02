0c8afb323ea2d61a04b88cda60e8ffb7a11e9eb0
Tiago Oliveira <contato@otao.dev.br>
2024-09-02 20:00:30 -0300

    feat: add a HomeController with GraphQl

--- graphql/src/HomeController.php
+++ graphql/src/HomeController.php
@@ -2,10 +2,71 @@
 
 namespace Otaodev\Infra;
 
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
+use GraphQL\Type\SchemaConfig;
+use RuntimeException;
+
 class HomeController
 {
     public function index(): string
     {
-        return "alguma coisa" . PHP_EOL;
+        try {
+            $queryType = new ObjectType([
+                'name' => 'Query',
+                'fields' => [
+                    'echo' => [
+                        'type' => Type::string(),
+                        'args' => [
+                            'message' => ['type' => Type::string()],
+                        ],
+                        'resolve' => static fn ($rootValue, array $args): string => $rootValue['prefix'] . $args['message'],
+                    ],
+                ],
+            ]);
+        
+            $mutationType = new ObjectType([
+                'name' => 'Mutation',
+                'fields' => [
+                    'sum' => [
+                        'type' => Type::int(),
+                        'args' => [
+                            'x' => ['type' => Type::int()],
+                            'y' => ['type' => Type::int()],
+                        ],
+                        'resolve' => static fn ($calc, array $args): int => $args['x'] + $args['y'],
+                    ],
+                ],
+            ]);
+        
+            // See docs on schema options:
+            // https://webonyx.github.io/graphql-php/schema-definition/#configuration-options
+            $schema = new Schema(
+                (new SchemaConfig())
+                ->setQuery($queryType)
+                ->setMutation($mutationType)
+            );
+        
+            $rawInput = file_get_contents('php://input');
+            if ($rawInput === false) {
+                throw new RuntimeException('Failed to get php://input');
+            }
+        
+            $input = json_decode($rawInput, true);
+            $query = $input['query'];
+            $variableValues = $input['variables'] ?? null;
+        
+            $rootValue = ['prefix' => 'You said: '];
+            $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+            $output = $result->toArray();
+
+            header('Content-Type: application/json; charset=UTF-8');
+            
+            return json_encode($output, JSON_THROW_ON_ERROR);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
